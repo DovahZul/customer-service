761a565f800956e27f247ba3c63f8cf252336e52
dovahzul <tanayris278@gmail.com>
2019-05-25 19:26:26 +0300

    wiping 2

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,53 +1,47 @@
-INSERT INTO vets VALUES (1, 'James', 'Carter');
-INSERT INTO vets VALUES (2, 'Helen', 'Leary');
-INSERT INTO vets VALUES (3, 'Linda', 'Douglas');
-INSERT INTO vets VALUES (4, 'Rafael', 'Ortega');
-INSERT INTO vets VALUES (5, 'Henry', 'Stevens');
-INSERT INTO vets VALUES (6, 'Sharon', 'Jenkins');
 
-INSERT INTO specialties VALUES (1, 'radiology');
-INSERT INTO specialties VALUES (2, 'surgery');
-INSERT INTO specialties VALUES (3, 'dentistry');
+INSERT INTO workers VALUES (1, 'Lyla', 'Ford');
+INSERT INTO workers VALUES (2, 'Lili', 'Estes');
+INSERT INTO workers VALUES (3, 'Priya', 'Harrison');
+INSERT INTO workers VALUES (4, 'Matias', 'Power');
+INSERT INTO workers VALUES (5, 'Brody', 'Garza');
+INSERT INTO workers VALUES (6, 'Willem', 'Herbert');
 
-INSERT INTO vet_specialties VALUES (2, 1);
-INSERT INTO vet_specialties VALUES (3, 2);
-INSERT INTO vet_specialties VALUES (3, 3);
-INSERT INTO vet_specialties VALUES (4, 2);
-INSERT INTO vet_specialties VALUES (5, 1);
+INSERT INTO customer_types VALUES (1, 'single');
+INSERT INTO customer_types VALUES (2, 'private');
+INSERT INTO customer_types VALUES (3, 'corporate');
+INSERT INTO customer_types VALUES (4, 'community');
+INSERT INTO customer_types VALUES (5, '+_+');
+INSERT INTO customer_types VALUES (6, '0_0');
+INSERT INTO customer_types VALUES (7, 'AAA');
 
-INSERT INTO types VALUES (1, 'cat');
-INSERT INTO types VALUES (2, 'dog');
-INSERT INTO types VALUES (3, 'lizard');
-INSERT INTO types VALUES (4, 'snake');
-INSERT INTO types VALUES (5, 'bird');
-INSERT INTO types VALUES (6, 'hamster');
+INSERT INTO currency_types VALUES (1, 'USD');
+INSERT INTO currency_types VALUES (2, 'EUR');
+INSERT INTO currency_types VALUES (3, 'RUB');
+INSERT INTO currency_types VALUES (4, 'UA');
+INSERT INTO currency_types VALUES (5, 'YEN');
+INSERT INTO currency_types VALUES (6, 'AU USD');
 
-INSERT INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023');
-INSERT INTO owners VALUES (2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749');
-INSERT INTO owners VALUES (3, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFarland', '6085558763');
-INSERT INTO owners VALUES (4, 'Harold', 'Davis', '563 Friendly St.', 'Windsor', '6085553198');
-INSERT INTO owners VALUES (5, 'Peter', 'McTavish', '2387 S. Fair Way', 'Madison', '6085552765');
-INSERT INTO owners VALUES (6, 'Jean', 'Coleman', '105 N. Lake St.', 'Monona', '6085552654');
-INSERT INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085555387');
-INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683');
-INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435');
-INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487');
+INSERT INTO customers VALUES (1, 'Kayden', 'Forster','PASSPORTSERIAL', '1867-5-5', '+12345678', 'NO MANS LAND ST.', 'ACTIVE', 1,2);
+INSERT INTO customers VALUES (2, 'Ismael', 'Mccall','PASSPORTSERIAL', '1867-5-5', '+12345678', 'NO MANS LAND ST.', 'ACTIVE', 2,1);
+INSERT INTO customers VALUES (3, 'Charity', 'Forrest','PASSPORTSERIAL', '1867-5-5', '+12345678', 'NO MANS LAND ST.', 'ACTIVE', 3,1);
+INSERT INTO customers VALUES (4, 'Hajrah', 'Buckner','PASSPORTSERIAL', '1867-5-5', '+12345678', 'NO MANS LAND ST.', 'ACTIVE', 4,1);
+INSERT INTO customers VALUES (5, 'Gwion', 'Powell','PASSPORTSERIAL', '1867-5-5', '+12345678', 'NO MANS LAND ST.', 'ACTIVE', 5,1);
+INSERT INTO customers VALUES (6, 'Atlas', 'Lester','PASSPORTSERIAL', '1867-5-5', '+12345678', 'NO MANS LAND ST.', 'ACTIVE', 5,1);
 
-INSERT INTO pets VALUES (1, 'Leo', '2010-09-07', 1, 1);
-INSERT INTO pets VALUES (2, 'Basil', '2012-08-06', 6, 2);
-INSERT INTO pets VALUES (3, 'Rosy', '2011-04-17', 2, 3);
-INSERT INTO pets VALUES (4, 'Jewel', '2010-03-07', 2, 3);
-INSERT INTO pets VALUES (5, 'Iggy', '2010-11-30', 3, 4);
-INSERT INTO pets VALUES (6, 'George', '2010-01-20', 4, 5);
-INSERT INTO pets VALUES (7, 'Samantha', '2012-09-04', 1, 6);
-INSERT INTO pets VALUES (8, 'Max', '2012-09-04', 1, 6);
-INSERT INTO pets VALUES (9, 'Lucky', '2011-08-06', 5, 7);
-INSERT INTO pets VALUES (10, 'Mulligan', '2007-02-24', 2, 8);
-INSERT INTO pets VALUES (11, 'Freddy', '2010-03-09', 5, 9);
-INSERT INTO pets VALUES (12, 'Lucky', '2010-06-24', 2, 10);
-INSERT INTO pets VALUES (13, 'Sly', '2012-06-08', 1, 10);
 
-INSERT INTO visits VALUES (1, 7, '2013-01-01', 'rabies shot');
-INSERT INTO visits VALUES (2, 8, '2013-01-02', 'rabies shot');
-INSERT INTO visits VALUES (3, 8, '2013-01-03', 'neutered');
-INSERT INTO visits VALUES (4, 7, '2013-01-04', 'spayed');
+INSERT INTO accounts VALUES (1, 3, 160, 1);
+INSERT INTO accounts VALUES (2, 3, 160, 1);
+INSERT INTO accounts VALUES (3, 3, 160, 1);
+INSERT INTO accounts VALUES (4, 3, 160, 1);
+INSERT INTO accounts VALUES (5, 3, 160, 1);
+INSERT INTO accounts VALUES (6, 3, 160, 1);
+INSERT INTO accounts VALUES (7, 1, 4266.3 ,1);
+INSERT INTO accounts VALUES (8, 2, 123 , 2);
+INSERT INTO accounts VALUES (9, 4, 234 , 2);
+INSERT INTO accounts VALUES (10, 6, 345 , 2);
+
+INSERT INTO credits VALUES (1, 1, 'Details about credit.', 0.3, 500, 200, '2010-09-07', '2014-09-07', true);
+
+INSERT INTO deposits VALUES (1, 1, 0.3, 500, 200, '2010-09-07', '2014-09-07', true);
+
+INSERT INTO houses VALUES (1, 'kadastr1', 'address1', 'district1', 'land1', 1536, 'material1', 'base1', 'comment1', 123.123, 30, 1.5, 2, 16, true, 5, 5, 3, 300, 40, 45, 50, 10, 444, 'document1', 'date_doc1', 'fio_host1', 'passport1', 0.4, 2005);
--- src/main/resources/db/hsqldb/schema.sql
+++ src/main/resources/db/hsqldb/schema.sql
@@ -1,64 +1,120 @@
-DROP TABLE vet_specialties IF EXISTS;
-DROP TABLE vets IF EXISTS;
-DROP TABLE specialties IF EXISTS;
-DROP TABLE visits IF EXISTS;
-DROP TABLE pets IF EXISTS;
+DROP TABLE customers IF EXISTS;
+DROP TABLE accounts IF EXISTS;
+DROP TABLE workers IF EXISTS;
+DROP TABLE credits IF EXISTS;
+DROP TABLE deposits IF EXISTS;
+
 DROP TABLE types IF EXISTS;
-DROP TABLE owners IF EXISTS;
 
-
-CREATE TABLE vets (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name  VARCHAR(30)
+CREATE TABLE houses (
+	
+	id INTEGER IDENTITY PRIMARY KEY, 
+	kadastr VARCHAR(20),
+	address VARCHAR(20),
+	disctrict VARCHAR(15),
+	land VARCHAR(10),
+	year INTEGER,
+	material VARCHAR(20),
+	base VARCHAR(20),
+	comment VARCHAR(50),
+	wear FLOAT,
+	flow INTEGER,
+	line FLOAT,
+	square FLOAT,
+	flats INTEGER,
+	elevator BOOLEAN,
+	flat INTEGER,
+	storey INTEGER,
+	rooms INTEGER,
+	square_flat FLOAT,
+	dwell FLOAT,
+	branch FLOAT,
+	balcony FLOAT,
+	height FLOAT,
+	recorrd INTEGER,
+	document VARCHAR(30),
+	date_doc VARCHAR(40),
+	fio_host VARCHAR(50),
+	passport VARCHAR(50),
+	part FLOAT,
+	born INTEGER
 );
-CREATE INDEX vets_last_name ON vets (last_name);
 
-CREATE TABLE specialties (
+
+CREATE TABLE customer_types (
   id   INTEGER IDENTITY PRIMARY KEY,
   name VARCHAR(80)
-);
-CREATE INDEX specialties_name ON specialties (name);
-
-CREATE TABLE vet_specialties (
-  vet_id       INTEGER NOT NULL,
-  specialty_id INTEGER NOT NULL
-);
-ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_vets FOREIGN KEY (vet_id) REFERENCES vets (id);
-ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_specialties FOREIGN KEY (specialty_id) REFERENCES specialties (id);
-
-CREATE TABLE types (
+ );
+ CREATE INDEX customer_types_name ON customer_types (name);
+ 
+ CREATE TABLE currency_types (
   id   INTEGER IDENTITY PRIMARY KEY,
   name VARCHAR(80)
-);
-CREATE INDEX types_name ON types (name);
+ );
+ CREATE INDEX currency_types_name ON currency_types (name);
 
-CREATE TABLE owners (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name  VARCHAR_IGNORECASE(30),
-  address    VARCHAR(255),
-  city       VARCHAR(80),
-  telephone  VARCHAR(20)
-);
-CREATE INDEX owners_last_name ON owners (last_name);
+CREATE TABLE workers (
 
-CREATE TABLE pets (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  name       VARCHAR(30),
-  birth_date DATE,
-  type_id    INTEGER NOT NULL,
-  owner_id   INTEGER NOT NULL
+id 			INTEGER IDENTITY PRIMARY KEY,
+first_name	VARCHAR(30),
+last_name	VARCHAR(30)
 );
-ALTER TABLE pets ADD CONSTRAINT fk_pets_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
-ALTER TABLE pets ADD CONSTRAINT fk_pets_types FOREIGN KEY (type_id) REFERENCES types (id);
-CREATE INDEX pets_name ON pets (name);
+CREATE INDEX workers_last_name ON workers (last_name);
 
-CREATE TABLE visits (
-  id          INTEGER IDENTITY PRIMARY KEY,
-  pet_id      INTEGER NOT NULL,
-  visit_date  DATE,
-  description VARCHAR(255)
+
+CREATE TABLE customers (
+id 			INTEGER IDENTITY PRIMARY KEY,
+first_name	VARCHAR(30),
+last_name	VARCHAR(30),
+passport_serial VARCHAR(20),
+birth_date DATE,
+mobile_phone VARCHAR(20),
+address VARCHAR(50),
+status VARCHAR(50),
+workers_id	INTEGER,
+type_id INTEGER NOT NULL
 );
-ALTER TABLE visits ADD CONSTRAINT fk_visits_pets FOREIGN KEY (pet_id) REFERENCES pets (id);
-CREATE INDEX visits_pet_id ON visits (pet_id);
+ALTER TABLE customers ADD CONSTRAINT fk_customer_helper FOREIGN KEY (workers_id) REFERENCES workers (id);
+
+CREATE INDEX customers_last_name ON customers (last_name);
+
+
+CREATE TABLE accounts(
+id INTEGER IDENTITY PRIMARY KEY,
+currency_type_id INTEGER NOT NULL,
+ballance DOUBLE NOT NULL,
+customer_id INTEGER
+);
+
+
+
+
+
+CREATE TABLE credits(
+id INTEGER PRIMARY KEY,
+account_id INTEGER NOT NULL,
+name VARCHAR(50),
+rate FLOAT NOT NULL,
+initial_balance DOUBLE NOT NULL,
+current_balance DOUBLE NOT NULL,
+take_date DATE NOT NULL,
+return_date DATE NOT NULL,
+status BOOLEAN NOT NULL
+);
+
+
+CREATE TABLE deposits(
+id INTEGER PRIMARY KEY,
+account_id INTEGER NOT NULL,
+rate FLOAT NOT NULL,
+initial_balance DOUBLE NOT NULL,
+current_balance DOUBLE NOT NULL,
+take_date DATE NOT NULL,
+return_date DATE NOT NULL,
+status BOOLEAN NOT NULL
+);
+
+
+
+
+
